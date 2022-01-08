8281738f6aade986e9449b9b60379ad99b38d706
Alaina <alaina.gossett@gmail.com>
2022-01-08 12:40:36 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+//Require Dependencies
+
+//Initalize the App
+
+//Configure Settings
+
+//Mount Middleware
+
+//Connect and Configure MongoDB
+
+//Define Models
+
+//Define Routes
+
+
+//Tell the app to listen
